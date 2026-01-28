515251a50855a5fbe6d0204c0d7403ce4f75c020
EkaSukhorukova <ees04@mail.ru>
2026-01-29 02:54:22 +0500

    to correctly retrieve movie details via DTO

--- app/src/main/java/com/example/cinema_sukhorukova_shift_2026/data/repository/MovieRepositoryImpl.kt
+++ app/src/main/java/com/example/cinema_sukhorukova_shift_2026/data/repository/MovieRepositoryImpl.kt
@@ -1,9 +1,11 @@
 package com.example.cinema_sukhorukova_shift_2026.data.repository
 
+import android.util.Log
 import com.example.cinema_sukhorukova_shift_2026.data.api.CinemaApi
 import com.example.cinema_sukhorukova_shift_2026.data.models.mapper.MovieMapper
 import com.example.cinema_sukhorukova_shift_2026.domain.entity.Movie
 import com.example.cinema_sukhorukova_shift_2026.domain.repository.MovieRepository
+import com.google.gson.Gson
 
 class MovieRepositoryImpl(private val api: CinemaApi) : MovieRepository {
 
@@ -17,7 +19,10 @@ class MovieRepositoryImpl(private val api: CinemaApi) : MovieRepository {
     }
 
     override suspend fun getMovie(id: String): Movie {
-        val filmDto = api.getFilm(id)
-        return MovieMapper.mapFilmDtoToMovie(filmDto)
+        val response = api.getFilm(id) // FilmsDetailDto
+        if (!response.success) {
+            throw Exception("Ошибка при получении фильма")
+        }
+        return MovieMapper.mapFilmDtoToMovie(response.film)
     }
 }
