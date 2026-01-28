9f1c577a84a841e3e3112945ccf0f20c160c442d
EkaSukhorukova <ees04@mail.ru>
2026-01-29 04:01:41 +0500

    add NavController holder

---
+++ app/src/main/java/com/example/cinema_sukhorukova_shift_2026/presentation/navigation/NavControllerHolder.kt
@@ -0,0 +1,7 @@
+package com.example.cinema_sukhorukova_shift_2026.presentation.navigation
+
+import androidx.navigation.NavHostController
+
+class NavControllerHolder {
+    var navController: NavHostController? = null
+}
