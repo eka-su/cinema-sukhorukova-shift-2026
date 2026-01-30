8f134d0d653b64800df2121c65d4f0aa9ffc6d45
EkaSukhorukova <ees04@mail.ru>
2026-01-30 13:29:01 +0500

    Create plug for unrealized screen

---
+++ app/src/main/java/com/example/cinema_sukhorukova_shift_2026/presentation/ui/screens/TicketsScreen.kt
@@ -0,0 +1,15 @@
+package com.example.cinema_sukhorukova_shift_2026.presentation.ui.screens
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable ///заглушка билетов
+fun TicketsScreen() {
+    Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text("Tickets Screen")
+    }
+}
