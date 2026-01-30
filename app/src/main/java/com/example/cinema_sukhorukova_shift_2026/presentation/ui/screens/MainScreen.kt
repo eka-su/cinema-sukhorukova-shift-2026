63547dd40ec6c6d909546e6fa32a0cbbe4f405a2
EkaSukhorukova <ees04@mail.ru>
2026-01-30 13:22:17 +0500

    add main scaffold + navigation host +  wrap theme

---
+++ app/src/main/java/com/example/cinema_sukhorukova_shift_2026/presentation/ui/screens/MainScreen.kt
@@ -0,0 +1,65 @@
+package com.example.cinema_sukhorukova_shift_2026.presentation.ui.screens
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.cinema_sukhorukova_shift_2026.presentation.navigation.NavControllerHolder
+import com.example.cinema_sukhorukova_shift_2026.presentation.navigation.NavigationOption
+import com.example.cinema_sukhorukova_shift_2026.presentation.ui.components.BottomBar
+import org.koin.compose.getKoin
+import androidx.compose.runtime.getValue
+
+@Composable
+fun MainScreen() {
+    val navController = rememberNavController()
+
+    // передаем navController в holder
+    val holder: NavControllerHolder = getKoin().get()
+    LaunchedEffect(Unit) {
+        holder.navController = navController
+    }
+
+    val currentBackStack by navController.currentBackStackEntryAsState()
+    val currentRoute = currentBackStack?.destination?.route
+
+    Scaffold(
+        bottomBar = {
+            BottomBar(
+                currentRoute = currentRoute,
+                onNavItemClick = { option ->
+                    navController.navigate(option.route) {
+                        popUpTo(navController.graph.startDestinationId) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                }
+            )
+        }
+    ) { padding ->
+        NavHost(
+            navController = navController,
+            startDestination = NavigationOption.Afisha.route,
+            modifier = Modifier.padding(padding)
+        ) {
+            composable(NavigationOption.Afisha.route) {
+                AfishaScreen()
+            }
+
+            composable(NavigationOption.Tickets.route) {
+                TicketsScreen()
+            }
+
+            composable(NavigationOption.Profile.route) {
+                ProfileScreen()
+            }
+        }
+    }
+}
