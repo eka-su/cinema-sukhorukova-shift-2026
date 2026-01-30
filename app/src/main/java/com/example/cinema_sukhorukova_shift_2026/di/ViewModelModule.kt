e72851d06a3c0b58c9db29b4a8d3bccd2cab64a0
EkaSukhorukova <ees04@mail.ru>
2026-01-30 14:37:12 +0500

    Add FilmViewModel for loading 1 movie

--- app/src/main/java/com/example/cinema_sukhorukova_shift_2026/di/ViewModelModule.kt
+++ app/src/main/java/com/example/cinema_sukhorukova_shift_2026/di/ViewModelModule.kt
@@ -2,11 +2,13 @@ package com.example.cinema_sukhorukova_shift_2026.di
 
 
 import com.example.cinema_sukhorukova_shift_2026.presentation.viewmodel.AfishaViewModel
+import com.example.cinema_sukhorukova_shift_2026.presentation.viewmodel.FilmViewModel
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val ViewModelModule = module {
     viewModel { AfishaViewModel(get(), get()) }
+    viewModel { FilmViewModel(get()) }
 }
 
 
